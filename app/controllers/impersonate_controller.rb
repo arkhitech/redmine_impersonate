16156613b7247395a4b6e0a577514d7943bacb5f
Babar Rehman <babar.rehman@arkhitech.com>
2013-12-18 15:47:48 +0500

    working refactored code

---
+++ app/controllers/impersonate_controller.rb
@@ -0,0 +1,31 @@
+class ImpersonateController < ApplicationController
+  unloadable
+  before_filter :project, :permission!
+  
+  def permission!
+    User.current.allowed_to?(:impersonate_project_user,project) || 
+      User.current_without_impersonation.allowed_to?(:impersonate_project_user,project)
+  end
+
+  def select_user
+    @users=@project.users
+  end
+  
+  def start_impersonation
+    user=User.find(params[:user][:impersonated_user_id])
+    puts "#{'*'*300} This is the user to be impersonated #{user.inspect}"
+    session[:impersonated_user_id]=params[:user][:impersonated_user_id]
+    User.impersonated_user=user
+    redirect_to action: 'select_user'
+  end
+  
+  def stop_impersonation
+    session[:impersonated_user_id] = User.impersonated_user = nil
+    redirect_to action: 'select_user'
+  end
+  
+  def project
+    @project = Project.find(params[:project_id])
+  end
+  private :project
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+resources :project do
+  match '/impersonate/start_impersonation', to: 'impersonate#start_impersonation',as: :start_impersonation , via:[:get,:post]
+  match '/impersonate/stop_impersonation', to: 'impersonate#stop_impersonation',as: :stop_impersonation, via:[:get,:post]
+  match '/impersonate/select_user', to: 'impersonate#select_user' ,as: :select_user, via:[:get,:post]
+end
